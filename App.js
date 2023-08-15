4dce476be607c4aef743eaed351f54d37d8ee2c0
hamzajaa <hamzajaa2017@gmail.com>
2023-08-16 00:18:39 +0100

    change 1

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import {Navigator} from './routes/homeStack'
+
+// const getFont = () => {
+//   return Font.loadAsync({
+//     'nunito-regular' : require('./assets/fonts/Nunito-Regular.ttf'),
+//     'nunito-bold' : require('./assets/fonts/Nunito-Bold.ttf')
+//   })
+// }
+
+
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+})
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Home /> // 3- show Home screen
+    );
+  } else {
+    return (
+      <Home/>
+      // <AppLoading
+      //   startAsync={getFonts}// 1- wait to get fonts
+      //   onFinish={() => setFontsLoaded(true)} // 2- set font
+      //   onError={(err)=>console.log(err)}
+      // />
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
---
+++ screens/about.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text } from "react-native";
+import { View } from "react-native";
+import { globalStyle } from "../styles/global";
+
+export default function About() {
+    return (
+        <View style={globalStyle.container}>
+            <Text>About Screen</Text>
+        </View>
+    )
+}
