64428dc92927a293f8c8cd22ce38d22693a0c530
hamzajaa <hamzajaa2017@gmail.com>
2023-08-16 21:19:27 +0100

    change 3.0

---
+++ routes/drawer.js
@@ -0,0 +1,21 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { AboutStack } from "./aboutStack";
+import { HomeStack } from "./homeStack"
+import { NavigationContainer } from "@react-navigation/native";
+import About from "../screens/about";
+import Home from "../screens/home";
+
+const Drawer = createDrawerNavigator();
+const RootDraweNavigator = () => {
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator screenOptions={{ headerShown: false }} >
+        <Drawer.Screen name="Home" component={HomeStack} />
+        <Drawer.Screen name="About" component={AboutStack} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default RootDraweNavigator; 
