cea731f39ae8b6f1c3daa7d3ee4a8b41fc0066d9
hamzajaa <hamzajaa2017@gmail.com>
2023-08-17 17:46:40 +0100

    change5.0

--- styles/global.js
+++ styles/global.js
@@ -15,6 +15,20 @@ export const globalStyle = StyleSheet.create({
         marginVertical: 8,
         lineHeight: 20,
     },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 10,
+        fontSize: 18,
+        borderRadius: 6,
+    },
+    errorText: {
+        color: 'crimson',
+        fontWeight: 'bold',
+        marginBottom: 10,
+        marginTop: 6,
+        textAlign: 'center,'
+    }
 });
 
 export const images = {
